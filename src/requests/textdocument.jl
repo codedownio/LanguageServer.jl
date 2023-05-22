8c1e699239a034880085c5aba84409a529eacf8f
Tom McLaughlin <tom@codedown.io>
2023-05-21 23:55:53 -0600

    Gate this thing

--- src/requests/textdocument.jl
+++ src/requests/textdocument.jl
@@ -149,7 +149,10 @@ function mark_errors(doc, out=Diagnostic[])
         line = 1
         io = IOBuffer(get_text(doc))
         while line < nlines
-            @info "Looking at err: $(errs[i])"
+            if i < length(errs)
+                @info "Looking at err: $(errs[i])"
+            end
+
             seek(io, line_offsets[line])
             char = 0
             while line_offsets[line] <= offset < line_offsets[line + 1]
